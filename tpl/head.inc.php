10afaf44fa40ec800bc585fbc6ce3088f5cd5a3f
burnbabyburn <xyz258@abwesend.de>
2019-07-02 01:12:11 +0200

    Mise à jour de 'tpl/head.inc.php'
    corrected links to favicons

--- tpl/head.inc.php
+++ tpl/head.inc.php
@@ -32,36 +32,36 @@ if (defined('TITLE')) {
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 <link rel="apple-touch-icon" sizes="57x57"
-    href="/icos/apple-touch-icon-57x57.png">
+    href="./icos/apple-touch-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60"
-    href="/icos/apple-touch-icon-60x60.png">
+    href="./icos/apple-touch-icon-60x60.png">
 <link rel="apple-touch-icon" sizes="72x72"
-    href="/icos/apple-touch-icon-72x72.png">
+    href="./icos/apple-touch-icon-72x72.png">
 <link rel="apple-touch-icon" sizes="76x76"
-    href="/icos/apple-touch-icon-76x76.png">
+    href="./icos/apple-touch-icon-76x76.png">
 <link rel="apple-touch-icon" sizes="114x114"
-    href="/icos/apple-touch-icon-114x114.png">
+    href="./icos/apple-touch-icon-114x114.png">
 <link rel="apple-touch-icon" sizes="120x120"
-    href="/icos/apple-touch-icon-120x120.png">
+    href="./icos/apple-touch-icon-120x120.png">
 <link rel="apple-touch-icon" sizes="144x144"
-    href="/icos/apple-touch-icon-144x144.png">
+    href="./icos/apple-touch-icon-144x144.png">
 <link rel="apple-touch-icon" sizes="152x152"
-    href="/icos/apple-touch-icon-152x152.png">
+    href="./icos/apple-touch-icon-152x152.png">
 <link rel="apple-touch-icon" sizes="180x180"
-    href="/icos/apple-touch-icon-180x180.png">
-<link rel="icon" type="image/png" href="/icos/favicon-32x32.png"
+    href="./icos/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="./icos/favicon-32x32.png"
     sizes="32x32">
-<link rel="icon" type="image/png" href="/icos/favicon-194x194.png"
+<link rel="icon" type="image/png" href="./icos/favicon-194x194.png"
     sizes="194x194">
-<link rel="icon" type="image/png" href="/icos/favicon-96x96.png"
+<link rel="icon" type="image/png" href="./icos/favicon-96x96.png"
     sizes="96x96">
 <link rel="icon" type="image/png"
-    href="/icos/android-chrome-192x192.png" sizes="192x192">
+    href="./icos/android-chrome-192x192.png" sizes="192x192">
 <link rel="icon" type="image/png" href="/icos/favicon-16x16.png"
     sizes="16x16">
-<link rel="manifest" href="/icos/manifest.json">
+<link rel="manifest" href="./icos/manifest.json">
 <meta name="msapplication-TileColor" content="#ffc40d">
-<meta name="msapplication-TileImage" content="/icos/mstile-144x144.png">
+<meta name="msapplication-TileImage" content="./icos/mstile-144x144.png">
 <meta name="theme-color" content="#ff0000">
 </head>
 <body>
