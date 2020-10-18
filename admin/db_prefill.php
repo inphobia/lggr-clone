96551dd1643880ff041b86f91e414002b7f1ecaa
kkretsch <kai@kaikretschmann.de>
2020-10-18 18:06:30 +0200

    Add simulation data support

---
+++ admin/db_prefill.php
@@ -0,0 +1,82 @@
+<?php
+namespace Lggr;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$faker = \Faker\Factory::create();
+$faker->seed(123);
+
+$arrayFacilities = array(
+	'kern','user','mail','daemon','auth','syslog','lpr','news','uucp','authpriv','ftp','cron','local0','local1','local2','local3','local4','local5','local6','local7'
+);
+$arrayLevels = array(
+	'emerg','alert','crit','err','warning','notice','info','debug'
+);
+$arrayServers = array();
+
+try {
+	$config = new Config();
+	$db = new \mysqli('localhost', $config->getDbUSer(),
+		            $config->getDbPwd(), $config->getDbName());
+	$db->set_charset('utf8');
+
+	trimAll($db);
+
+	$arrayServers = makeServers($faker, $db, 5);
+	makeEntries($faker, $db, 200);
+}
+catch (LggrException $e) {
+  die($e->getMessage());
+} // try
+
+function trimAll($db) {
+	$db->query("TRUNCATE TABLE servers");
+	$db->query("TRUNCATE TABLE newlogs");
+}
+
+function makeServers($faker, $db, $count=10) {
+	$a = array();
+	for($i=0; $i<$count; $i++) {
+		$name = $faker->domainWord;
+		echo $name . ", ";
+		$sql = "INSERT INTO servers (name) VALUES ('" . $name . "')";
+		$db->query($sql);
+		$id = $db->insert_id;
+		$a[$id] = $name;
+	}
+	return $a;
+}
+
+function makeEntries($faker, $db, $count=1000) {
+	global $arrayFacilities, $arrayLevels, $arrayServers;
+
+	if (!($stmt = $db->prepare("INSERT INTO newlogs (date, facility, level, host, program, pid, message, idhost) VALUES (?,?,?,?,?,?,?,?)"))) {
+		    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	$dDate = "";
+	$dFacility = "";
+	$dLevel = "";
+	$dHost = "";
+	$dProgram = "";
+	$dPid = 0;
+	$dMessage = "";
+	$dIdhost = 0;
+	if (!$stmt->bind_param("sssssisi", $dDate,$dFacility,$dLevel,$dHost,$dProgram,$dPid,$dMessage,$dIdhost)) {
+		    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+
+	for($i=0; $i<$count; $i++) {
+		$dDate = $faker->dateTime('2020-12-24 11:22:33')->format('Y-m-d H:i:s');
+		$dFacility = array_rand($arrayFacilities);
+		$dLevel = array_rand($arrayLevels);
+		$dProgram = $faker->domainWord;
+		$dPid = rand(1, 65500);
+		$dMessage = $faker->text;
+		$dIdhost = array_rand($arrayServers);
+		$dHost = $arrayServers[$dIdhost];
+
+		$stmt->execute();
+	}
+	$stmt->close();
+}
+
