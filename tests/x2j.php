bf7fc43f651f2e46c58ff5d46087774030e476de
kkretsch <kai@kaikretschmann.de>
2020-11-12 15:35:31 +0100

    Fix json numeric values

--- tests/x2j.php
+++ tests/x2j.php
@@ -3,5 +3,5 @@
 $xfile = file_get_contents(__DIR__ . '/../logs/junit.xml');
 $xml = simplexml_load_string($xfile);
 $xml->testsuite->addAttribute('timestamp', date(DATE_ISO8601));
-$json = json_encode($xml);
+$json = json_encode($xml, JSON_NUMERIC_CHECK );
 echo $json;
