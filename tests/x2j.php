5473281827376a05c19c6557b41fd83297e7994f
kkretsch <kai@kaikretschmann.de>
2020-11-12 13:47:11 +0100

    Add timestamp to report file

--- tests/x2j.php
+++ tests/x2j.php
@@ -2,5 +2,7 @@
 
 $xfile = file_get_contents(__DIR__ . '/../logs/junit.xml');
 $xml = simplexml_load_string($xfile);
+$xml->testsuite->addAttribute('timestamp', date(DATE_ISO8601));
+print_r($xml); exit;
 $json = json_encode($xml);
 echo $json;
