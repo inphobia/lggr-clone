7d2fe7cc2bd862182d2beb5fcf266db3d3ee0815
Kai Kretschmann <1-kai@users.noreply.gitlab.kretschmann.software>
2020-11-16 21:09:20 +0100

    Update TesterTest.php

--- tests/TesterTest.php
+++ tests/TesterTest.php
@@ -7,24 +7,28 @@ require __DIR__ . '/../vendor/autoload.php';
 final class TesterTest extends TestCase {
     protected static $cfg;
 
-    public static function setUpBeforeClass() 	{
+    public static function setUpBeforeClass(): void
+    {
         self::$cfg = new \Lggr\Config(); 
     }
 
-    public function testIncomplete() 	{
+    public function testIncomplete(): void
+    {
         $this->markTestIncomplete(
           'This test has not been implemented yet.'
         );
     }
 
-    public function testSkipped() 	{
+    public function testSkipped(): void
+    {
         $this->markTestSkipped(
           'This test has been skipped.'
         );
     }
 
     // check error handling, skipped because of prefix
-    public function tstFailure() 	{
+    public function tstFailure(): void
+    {
     	$this->assertEquals('abc', '123');
     }
 }
