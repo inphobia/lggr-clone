4e7cc4ba3188fa73dfbbf6b366556f061c1d209a
Kai KRETSCHMANN <kai@kretschmann.consulting>
2020-11-29 16:21:56 +0100

    add REPLACE for user creation

--- doc/user.sql
+++ doc/user.sql
@@ -1,7 +1,7 @@
 # create the following different mysql users:
 
-CREATE USER lggrauth@'%' IDENTIFIED BY 'xxx';
-CREATE USER lggrauth@localhost IDENTIFIED BY 'xxx';
+CREATE OR REPLACE USER lggrauth@'%' IDENTIFIED BY 'xxx';
+CREATE OR REPLACE USER lggrauth@localhost IDENTIFIED BY 'xxx';
 GRANT SELECT ON lggr.phpauth_config TO lggrauth@'%';
 GRANT SELECT ON lggr.phpauth_config TO lggrauth@localhost;
 GRANT INSERT,SELECT,DELETE,UPDATE ON lggr.phpauth_attempts TO lggrauth@'%';
@@ -14,27 +14,27 @@ GRANT INSERT,SELECT,DELETE,UPDATE ON lggr.phpauth_emails_banned TO lggrauth@'%';
 GRANT INSERT,SELECT,DELETE,UPDATE ON lggr.phpauth_emails_banned TO lggrauth@localhost;
 
 # used by syslog-ng for inserting new data, referenced in /etc/syslog-ng/conf.d/08newlogsql.conf
-CREATE USER lggrsyslog@'%' IDENTIFIED BY 'xxx'
-CREATE USER lggrsyslog@localhost IDENTIFIED BY 'xxx';
+CREATE OR REPLACE USER lggrsyslog@'%' IDENTIFIED BY 'xxx'
+CREATE OR REPLACE USER lggrsyslog@localhost IDENTIFIED BY 'xxx';
 GRANT INSERT ON lggr.newlogs TO lggrsyslog@'%';
 GRANT INSERT ON lggr.newlogs TO lggrsyslog@localhost;
 
 # used by the web gui for normal viewing, referenced in inc/config_class.php
-CREATE USER lggrweb@'%' IDENTIFIED BY 'xxx';
-CREATE USER lggrweb@localhost IDENTIFIED BY 'xxx';
+CREATE OR REPLACE USER lggrweb@'%' IDENTIFIED BY 'xxx';
+CREATE OR REPLACE USER lggrweb@localhost IDENTIFIED BY 'xxx';
 GRANT SELECT ON lggr.* TO lggrweb@'%';
 GRANT SELECT ON lggr.* TO lggrweb@localhost;
 
 # used by clean up cron job and for archiving, referenced in inc/adminconfig_class.php
-CREATE USER lggrcron@'%' IDENTIFIED BY 'xxx';
-CREATE USER lggrcron@localhost IDENTIFIED BY 'xxx';
+CREATE OR REPLACE USER lggrcron@'%' IDENTIFIED BY 'xxx';
+CREATE OR REPLACE USER lggrcron@localhost IDENTIFIED BY 'xxx';
 GRANT SELECT,DELETE ON lggr.newlogs TO lggrcron@'%';
 GRANT SELECT,DELETE ON lggr.newlogs TO lggrcron@localhost;
 GRANT SELECT,INSERT,DELETE  ON TABLE lggr.servers TO lggrcron@'%';
 GRANT SELECT,INSERT,DELETE  ON TABLE lggr.servers TO lggrcron@localhost;
 
 # used for CI, skip for live setup
-CREATE USER lggrci@'%' IDENTIFIED BY 'xxx';
+CREATE OR REPLACE USER lggrci@'%' IDENTIFIED BY 'xxx';
 GRANT ALL ON lggr.* TO lggrci@'%';
 
 # activate changes
