f81378a146dac32b8378e517aaf631afe736efd1
kkretsch <kai@kaikretschmann.de>
2021-01-16 09:10:23 +0100

    Fix auth attribute access on login

--- dologin.php
+++ dologin.php
@@ -7,10 +7,10 @@ $user = $_POST['authEmail'];
 $pwd = $_POST['authPassword'];
 $o = $auth->login($user, $pwd, true);
 
-if($o->error) {
+if($o['error']) {
 	// TODO
 } else {
-	$_SESSION['authHash'] = $o->hash;
+	$_SESSION['authHash'] = $o['hash'];
 	setcookie($authConfig->cookie_name, $o['hash'], $o['expire'], $authConfig->cookie_path, $authConfig->cookie_domain, $authConfig->cookie_secure, $authConfig->cookie_http);
 	header('Location: index.php');
 }
