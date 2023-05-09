ed9ee47b3976b0481257665f72aaca53e86df7f3
Michael Kretschmann <kaikretschmann@gmail.com>
2023-05-09 07:01:31 +0000

    sona clean

--- admin/auth_register.php
+++ admin/auth_register.php
@@ -22,43 +22,36 @@ $parser = new OptionParser($specs);
 
 $u_email = null;
 $u_password = null;
-try
-{
+try {
 	$result = $parser->parse($argv);
 
-	if(!array_key_exists('email', $result->keys)) {
+	if (!array_key_exists('email', $result->keys)) {
 		throw new InvalidArgumentException('email missing');
 	}
-	if(!array_key_exists('password', $result->keys)) {
+	if (!array_key_exists('password', $result->keys)) {
 		throw new InvalidArgumentException('password missing');
 	}
 
 	$u_email = $result->keys['email']->value;
 	$u_password = $result->keys['password']->value;
-}
-catch(Exception $e)
-{
+} catch (Exception $e) {
 	echo $e->getMessage() . "\n\n";
 	echo $printer->render($specs);
 	exit(1);
 }
 
 $dbh = new PDO(
-	"mysql:host=" . $config->getDbHost() .
-	";dbname=" . $config->getDbName(),
-	$config->getDbUser(),
-	$config->getDbPwd()
+    "mysql:host=" . $config->getDbHost() . ";dbname=" . $config->getDbName(),
+    $config->getDbUser(),
+    $config->getDbPwd()
 );
 $authconfig = new PHPAuthConfig($dbh);
 $auth = new PHPAuth($dbh, $authconfig);
 
-$o = $auth->register($u_email, $u_password, $u_password, Array(), null, false);
-if($o['error'])
-{
+$o = $auth->register($u_email, $u_password, $u_password, array(), null, false);
+if ($o['error']) {
 	echo $o['message'] . "\n";
 	exit(2);
-}
-else
-{
+} else {
 	echo "OK\n";
 }
